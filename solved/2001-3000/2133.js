b3d58861e9ee1f8350532244e18a2e7a820fac21
euichan <ecjung@jlkgroup.com>
2025-11-24 20:40:06 +0900

    solved: 2133

---
+++ solved/2001-3000/2133.js
@@ -0,0 +1,19 @@
+const input = +require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim()
+
+const dp = Array(31).fill(0)
+dp[2] = 3
+
+for(let i = 4; i<31; i+=2) {
+    let result = 0
+    result += dp[i-2] * dp[2]
+    for(let j = i-4; j>=0; j-=2) {
+        result += dp[j] *2
+    }
+    result += 2
+    dp[i] = result
+}
+
+console.log(dp[input])
