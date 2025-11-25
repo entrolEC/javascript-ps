42308e464ccbe6a8d114efe98b27fec65d9bce9f
entrolEC <euichan1108@gmail.com>
2025-11-25 09:05:34 +0900

    solved: 2133

--- solved/2001-3000/2133.js
+++ solved/2001-3000/2133.js
@@ -4,6 +4,7 @@ const input = +require("fs")
     .trim()
 
 const dp = Array(31).fill(0)
+dp[0] = 1
 dp[2] = 3
 
 for(let i = 4; i<31; i+=2) {
@@ -12,7 +13,6 @@ for(let i = 4; i<31; i+=2) {
     for(let j = i-4; j>=0; j-=2) {
         result += dp[j] *2
     }
-    result += 2
     dp[i] = result
 }
 
